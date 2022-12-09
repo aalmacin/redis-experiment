62ae11567c5aaae3652bf26fe9ce0c26d26dbf43
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2022-12-09 16:35:20 -0600

    Simple message test

--- src/main/java/RedisExperiment.java
+++ src/main/java/RedisExperiment.java
@@ -7,24 +7,29 @@ import java.util.List;
 public class RedisExperiment {
 
     public static void main(String[] args) {
+        System.out.println("Start");
         RedisClient redisClient = new RedisClient(RedisURI.create("redis://10.98.144.67:6379"));
 
         RedisConnection<String, String> redisConnection = redisClient.connect();
 
-        redisConnection.setex("banana1", 300, "100");
-        redisConnection.setex("vm1", 300, "100");
-        redisConnection.setex("vm2", 300, "58");
-        redisConnection.setex("vm3", 300, "38");
-
-        List<String> keys = redisConnection.keys("vm*");
-        keys.forEach(k -> {
-            String s = redisConnection.get(k);
-            System.out.println("Value: " + s);
-        });
-
-        redisConnection.del("banana1", "vm1", "vm2", "vm3");
+        System.out.println("Connected to redis");
+//
+//        redisConnection.setex("banana1", 300, "100");
+//        redisConnection.setex("vm1", 300, "100");
+//        redisConnection.setex("vm2", 300, "58");
+//        redisConnection.setex("vm3", 300, "38");
+//
+//        List<String> keys = redisConnection.keys("vm*");
+//        keys.forEach(k -> {
+//            String s = redisConnection.get(k);
+//            System.out.println("Value: " + s);
+//        });
+//
+//        redisConnection.del("banana1", "vm1", "vm2", "vm3");
 
         redisConnection.close();
         redisClient.shutdown();
+
+        System.out.println("Done");
     }
 }
